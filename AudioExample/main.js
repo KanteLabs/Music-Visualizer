22a80d26beb605cde8db321249c605fc3cf8cda5
Amidou Kante <kamdiou@gmail.com>
2017-09-06 17:54:19 -0400

    can upload and play audio

--- AudioExample/main.js
+++ AudioExample/main.js
@@ -2,6 +2,12 @@ window.onload = function() {
 var audioFile = document.querySelector('#audioFile');
 var audioPlayer = document.querySelector('#audio');
 
+audioFile.onchange = (event) =>{
+    audioFile = event.target.files;
+    console.log(`Now playing audio ${audioFile[0].name}`)
+    //Creates a temporary url for the file input in this case the audio that is being uploaded
+    audioPlayer.src = URL.createObjectURL(audioFile[0])
+}
 // AnalyserNode is necessary to provide real-time frequency and time-domain analysis information. It is an AudioNode that passes the audio stream unchanged from the input to the output, but allows you to take the generated data, process it, and create audio visualizations.
 var audioCtx = new(window.AudioContext || window.webkitAudioContext)();
 var analyser = audioCtx.createAnalyser();
@@ -9,7 +15,7 @@ var analyser = audioCtx.createAnalyser();
 updateSource=(event)=>{
     console.log(event.target.files[0].name)
     var songName = event.target.files[0].name;
-    var newSource = 
+    var newSource = "Cat"
 }
 source = audioCtx.createMediaStreamSource
 }
